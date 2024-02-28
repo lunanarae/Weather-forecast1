659d465c2f2e93a6026149b8effb2cc907b86f18
lunanarae <lunanarae@gmail.com>
2024-02-28 20:54:43 +0800

    add current-time JS

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ function currentWeatherDetails(response) {
   let temperatureElement = docuament.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#location");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
 
   console.log(response.data);
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
+  timeElement.innerHTML = formatDate(date);
 }
 
 function search(event) {
@@ -19,6 +22,27 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentWeatherDetails);
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
